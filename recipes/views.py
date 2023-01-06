571eed5cbc085a609151a8ef3fc27de89c98b82c
Matheus Carvalho <matheusluancarvalho@gmail.com>
2023-01-06 16:45:59 -0300

    Seção 5 - Aula 28

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
+    return render(request, 'recipes/pages/home.html', context={
+        'nome': 'Matheus',
+    })
+    
