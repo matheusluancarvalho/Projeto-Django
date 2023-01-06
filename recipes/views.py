d5b12c4ba893f35e4421af3f9f685dc0d42d6ea3
Matheus Carvalho <matheusluancarvalho@gmail.com>
2023-01-06 09:29:40 -0300

    Movendo as URLs para o views e URLs na recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('Home 1')
+
+
+def sobre(request):
+    return HttpResponse('Sobre1')
+
+
+def contato(request):
+    return HttpResponse('Contato1')
